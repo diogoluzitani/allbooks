6cbc223d883762610ae9f8cb7cec4dbac0b20a77
Diogo Luzitani <dyoluzitani@gmail.com>
2026-03-16 21:35:49 -0400

    Teste

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ server.use('/public', publicRoutes) //rotas na porta 8000
 
 server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddleware);
 
-server.use(router) //rotas na prota 3000
+server.use(router) //rotas na prota 3000 999
 
 server.listen(8000, () => {
   console.log("API disponível em http://localhost:8000")
